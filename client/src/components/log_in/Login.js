2191acff4c5045d066ff933b2c78759a16333bdf
hiepuet1705 <hiephap1705@gmail.com>
2024-07-01 22:49:47 +0700

    fix

--- client/src/components/log_in/Login.js
+++ client/src/components/log_in/Login.js
@@ -2,76 +2,64 @@
 
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import './login.css';
-import AuthContext from '../variable/AuthContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import Cookies from 'js-cookie';
-
-
+import "./login.css";
+import AuthContext from "../variable/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const { setRole, setUser } = useContext(AuthContext);
   const [showPassword, setShowPassword] = useState(false);
 
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers:
-        {
-          'Content-Type': 'application/json',
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
-      })
+      });
       const data = await response.json();
-      console.log("login data" ,data);
-      // const uid = data.uid;
-      // Cookies.set('uid', uid, { expires: 1, path: '/' });
-      // console.log(Cookies.get('uid'));
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('pointID', data.pointID);
-      localStorage.setItem('idArea', data.idArea);
-      localStorage.setItem('province', data.province);
-      localStorage.setItem('district', data.district);
-      // const uidCookie = response.headers.get('set-cookie');
-      // console.log(uidCookie);
-      // console.log(data)
+      console.log("login data", data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("pointID", data.pointID);
+      localStorage.setItem("idArea", data.idArea);
+      localStorage.setItem("province", data.province);
+      localStorage.setItem("district", data.district);
       if (response.ok) {
         setRole(data.role);
         setUser(username);
         console.log(data);
         // const uid = data.uid;
         if (data.role === "manager") {
-          navigate('/admin');
+          navigate("/admin");
         } else if (data.role === "warehouse leader") {
-          navigate('/warehouse');
+          navigate("/warehouse");
         } else if (data.role === "transaction leader") {
-          navigate('/transactionpoint');
+          navigate("/transactionpoint");
         } else if (data.role === "transaction staff") {
-          navigate('/tellermain');
+          navigate("/tellermain");
         } else if (data.role === "warehouse staff") {
-          navigate('/warehousestaff');
+          navigate("/warehousestaff");
         } else {
-          navigate('/user');
+          navigate("/user");
         }
       } else {
         setError(data.error);
       }
     } catch (error) {
-      setError('An error occurred. Please try again.');
+      setError("An error occurred. Please try again.");
     }
   };
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
 
   return (
     <form className="login" onSubmit={handleSubmit}>
@@ -92,7 +80,7 @@ const Login = () => {
         <label>Password</label>
         <div className="password-input-container">
           <input
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             name="password"
             className="form-control"
             placeholder="Enter password"
@@ -103,11 +91,14 @@ const Login = () => {
             className="toggle-password-btn"
             onClick={togglePasswordVisibility}
           >
-            {showPassword ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
+            {showPassword ? (
+              <FontAwesomeIcon icon={faEyeSlash} />
+            ) : (
+              <FontAwesomeIcon icon={faEye} />
+            )}
           </label>
         </div>
       </div>
-
 
       <div className="mb-3">
         <div className="custom-control custom-checkbox">
@@ -116,18 +107,22 @@ const Login = () => {
             className="custom-control-input"
             id="customCheck1"
           />
-          <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
         </div>
       </div>
       {error && <div className="alert alert-danger">{error}</div>}
       <div className="d-grid">
-        <button type="submit" className="btn btn-primary">Đăng Nhập</button>
+        <button type="submit" className="btn btn-primary">
+          Đăng Nhập
+        </button>
       </div>
       <p className="forgot-password text-right">
         Forgot <a href="#">password?</a>
       </p>
     </form>
   );
-}
+};
 
 export default Login;
